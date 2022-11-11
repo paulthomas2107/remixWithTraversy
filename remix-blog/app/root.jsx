47288de6dd525c45d714a5b7ab611c87290b1b51
Paul Thomas <paulthomas2107@gmail.com>
2022-11-11 11:48:16 +0000

    first commit

---
+++ remix-blog/app/root.jsx
@@ -0,0 +1,43 @@
+import { Outlet, LiveReload, Link } from '@remix-run/react';
+
+export default function App() {
+  return (
+    <Document>
+      <Layout>
+        <Outlet />
+      </Layout>
+    </Document>
+  );
+}
+
+function Document({ children, title }) {
+  return (
+    <html lang="en">
+      <head>
+        <title>{title ? title : 'Remix Blog...'}</title>
+      </head>
+      <body>
+        {children}
+        {process.env.NODE_ENV === 'development' ? <LiveReload /> : null}
+      </body>
+    </html>
+  );
+}
+
+function Layout({ children }) {
+  return (
+    <>
+      <nav className="navbar">
+        <Link to="/" className="logo">
+          Remix
+        </Link>
+        <ul className="nav">
+          <li>
+            <Link to="/posts">Posts</Link>
+          </li>
+        </ul>
+      </nav>
+      <div className="containerr">{children}</div>
+    </>
+  );
+}
