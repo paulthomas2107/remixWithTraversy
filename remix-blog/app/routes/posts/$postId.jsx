afde9ed3153270e21d10fb3ef9163ca66d8cc30a
Paul Thomas <paulthomas2107@gmail.com>
2022-11-11 15:33:29 +0000

    Complete

--- remix-blog/app/routes/posts/$postId.jsx
+++ remix-blog/app/routes/posts/$postId.jsx
@@ -1,36 +1,55 @@
-import { useLoaderData, Link } from "@remix-run/react"
-import { db } from  '~/utils/db.server'
+import { useLoaderData, Link } from '@remix-run/react';
+import { redirect } from '@remix-run/node';
+import { db } from '~/utils/db.server';
 
-export const loader = async ({params}) => {
+export const loader = async ({ params }) => {
   const post = await db.post.findUnique({
-    where: { id: params.postId }
-  })
+    where: { id: params.postId },
+  });
 
-  if (!post) throw new Error('Post not found')
+  if (!post) throw new Error('Post not found');
 
-  const data = {post}
+  const data = { post };
 
-  return data
+  return data;
+};
 
+export const action = async ({request, params}) => {
+  const form = await request.formData();
 
-}
+  if (form.get('_method') === 'delete') {
+    const post = await db.post.findUnique({
+      where: { id: params.postId },
+    });
+
+    if (!post) throw new Error('Post not found');
+
+    await db.post.delete({ where: { id: params.postId}})
+
+    return redirect('/posts')
+  }
+};
 
 function Post() {
+  const { post } = useLoaderData();
 
-    const {post} = useLoaderData()
-
-    return (
-      <div>
-        <div className="page-header">
+  return (
+    <div>
+      <div className="page-header">
         <h1>{post.title}</h1>
-        <Link to="/posts" className="btn btn-reverse">Back</Link>
-        </div>
-        <div className="page-content">
-          {post.body}
-        </div>
+        <Link to="/posts" className="btn btn-reverse">
+          Back
+        </Link>
       </div>
-    )
-  }
-  
-  export default Post
-  
+      <div className="page-content">{post.body}</div>
+      <div className="page-footer">
+        <form method="POST">
+          <input type="hidden" name="_method" value="delete" />
+          <button className="btn btn-delete">Delete</button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Post;
